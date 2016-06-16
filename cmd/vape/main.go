065a24ff079f65611d0301a6080544a7e34eb4be
Andrew Wansley <andrew.wansley@gmail.com>
2016-06-15 23:04:38 -0700

    Initial commit

---
+++ cmd/vape/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
+
+	"github.com/blevesearch/bleve"
+	"github.com/docopt/docopt-go"
+)
+
+const usage = `vape
+
+Usage:
+  vape index <json_file>
+	vape search <query>
+	vape forever`
+
+const indexDir = "index"
+
+func createOrGetIndex(id string) (bleve.Index, error) {
+	p := path.Join(indexDir, id)
+	index, err := bleve.Open(p)
+	if err == nil {
+		return index, nil
+	}
+	mapping := bleve.NewIndexMapping()
+	index, err = bleve.New(p, mapping)
+	if err != nil {
+		return nil, err
+	}
+	return index, nil
+}
+
+var providerIDs = []string{"1", "2"}
+
+func main() {
+	args, _ := docopt.Parse(usage, nil, true, "V0", false)
+	if args["index"].(bool) {
+		filename := args["<json_file>"].(string)
+		bytes, err := ioutil.ReadFile(filename)
+		var jsonList []interface{}
+
+		err = json.Unmarshal(bytes, &jsonList)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		indexMap := make(map[string]bleve.Index)
+		for _, providerID := range providerIDs {
+			index, err := createOrGetIndex(providerID)
+			if err != nil {
+				log.Fatal(err)
+			}
+			indexMap[providerID] = index
+		}
+
+		for _, obj := range jsonList {
+			doc := obj.(map[string]interface{})
+			providerID := doc["provider_id"].(string)
+			docID := doc["id"].(string)
+			index := indexMap[providerID]
+			index.Index(docID, doc)
+		}
+	}
+
+	if args["search"].(bool) {
+		q := args["<query>"].(string)
+
+		mergedIndex := bleve.NewIndexAlias()
+		for _, providerID := range providerIDs {
+			p := path.Join(indexDir, providerID)
+			index, err := bleve.Open(p)
+			if err != nil {
+				log.Fatal(err)
+			}
+			mergedIndex.Add(index)
+		}
+		query := bleve.NewMatchQuery(q)
+		search := bleve.NewSearchRequest(query)
+		hits, err := mergedIndex.Search(search)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(hits)
+	}
+}
